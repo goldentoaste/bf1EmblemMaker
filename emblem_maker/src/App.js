d614608a89c1d384772ccf9be822969a5b219c79
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2022-01-15 12:43:35 -0700

    Backbone Structure and CSS Structures
    a whole bunch of divs

--- emblem_maker/src/App.js
+++ emblem_maker/src/App.js
@@ -1,12 +1,59 @@
 import React from 'react';
+import './App.css';
 
 
 function App() {
 
     return (
-        <h1>Hello</h1>
-    )
-}
+        
+        
+        <body>
+            <div className='main'>
+                
+                <div className='canvas'>
+
+                    <p>Placeholder for canvas</p> 
+
+                </div>
+                
+                
+                
+                <div className='position'>
+
+                    <div className='border rounded m-3 p-5 shadow bg-warning'>
+                        <p>X Position and Y Position</p>
+                    </div>
+                    <div className='border rounded m-3 p-5 shadow bg-warning'>
+                        <p>Height and Width</p>
+                    </div>
+                    <div className='width'>
+
+                    </div>
 
 
+
+
+                </div>
+
+
+                <div className='shapes'>
+                    <p>Placeholder for shapes</p>
+
+                </div>
+
+                <div className='list'>
+                    <p>Placeholder for list</p>
+
+                </div>
+
+
+
+
+            </div>
+    
+
+            
+        </body>
+        )
+    }
 export default App;
